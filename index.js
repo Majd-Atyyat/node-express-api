99ca4c2ddbd9ada0ad7db1cc7eefb02f7cef23c0
majd atyyat <majd.atyyat@pwc.com>
2023-04-02 07:53:45 +0300

    express.js api with prisma

--- index.js
+++ index.js
@@ -1,92 +1,70 @@
+
+// Import the required dependencies
 const express = require('express');
-const bodyParser = require('body-parser');
-const pg = require('pg');
+const { PrismaClient } = require('@prisma/client');
+const cors = require('cors');
 
+//Create a new instance of the Express app:
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-const config = {
-   user: "postgres",
-    password: "zatar123!",
-    host: "interns.postgres.database.azure.com",
-    port: 5432,
-    database: "majd",
-};
+//Create a new instance of the Prisma client:
+const prisma = new PrismaClient();
 
-const pool = new pg.Pool(config);
+app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// ROUTES//
 
-// GET all items
-app.get('/items', (req, res) => {
-  pool.query('SELECT * FROM items', (err, result) => {
-    if (err) {
-      throw err;
-    }
-
-    res.send(result.rows);
-  });
+//Retrieve all items:
+app.get('/items', async (req, res) => {
+  const items = await prisma.user.findMany();
+  res.json(items);
 });
 
-// GET one item by id
-app.get('/items/:id', (req, res) => {
-  const id = req.params.id;
-
-  pool.query(`SELECT * FROM items WHERE id = ${id}`, (err, result) => {
-    if (err) {
-      throw err;
-    }
-
-    res.send(result.rows);
-  });
+//Retrieve a single user by ID
+app.get('/items/:id', async (req, res) => {
+  const { id } = req.params;
+  const item = await prisma.user.findMany({ where: { id: parseInt(id) } });
+  if (!item) {
+    return res.status(404).json({ message: 'Item not found' });
+  }
+  res.json(item);
 });
 
-// POST a new item
-app.post('/items', (req, res) => {
+//Create a new item
+
+app.post('/items', async (req, res) => {
   const { name, description, price } = req.body;
-
-  pool.query(
-    `INSERT INTO items (name, description, price) VALUES ('${name}', '${description}', ${price})`,
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-
-      res.send(`Item added with ID: ${result.insertId}`);
-    }
-  );
-});
-
-// PUT (update) an existing item
-app.put('/items/:id', (req, res) => {
-  const id = req.params.id;
-  const { name, description, price } = req.body;
-
-  pool.query(
-    `UPDATE items SET name = '${name}', description = '${description}', price = ${price} WHERE id = ${id}`,
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-
-      res.send(`Item with ID: ${id} updated`);
-    }
-  );
-});
-
-// DELETE an existing item
-app.delete('/items/:id', (req, res) => {
-  const id = req.params.id;
-
-  pool.query(`DELETE FROM items WHERE id = ${id}`, (err, result) => {
-    if (err) {
-      throw err;
-    }
-
-    res.send(`Item with ID: ${id} deleted`);
+  const newItem = await prisma.item.create({
+    data: { name, description, price },
   });
+  res.json(newItem);
 });
+
+//  Updating an item by ID
+app.put('/items/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const item = await prisma.item.update({ where: { id } });
+  if (!item) {
+    return res.status(404).json({ error: 'Item not found' });
+  }
+  const updatedItem = await prisma.item.update({ where: { id }, data: { name } });
+  res.json(updatedItem);
+});
+
+// DELETE endpoint for deleting an item by ID
+app.delete('/items/:id', async (req, res) => {
+  const { id } = req.params;
+  const item = await prisma.item.delete({ where: { id } });
+  if (!item) {
+    return res.status(404).json({ error: 'Item not found' });
+  }
+  await prisma.item.delete({ where: { id } });
+  res.status(204).send();
+});
+
 
 // Start the server
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
