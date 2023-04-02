fb0ec6b3575822aa07ecee9ee7116033f631cebf
majd atyyat <majd.atyyat@pwc.com>
2023-04-02 05:53:46 +0300

    changed the table , routes, deleted the env file

--- schema.sql
+++ schema.sql
@@ -1,9 +1,6 @@
-CREATE TABLE employee (
-    emp_id 	INT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    gender CHAR(1),
-    birthdate DATE,
-    email VARCHAR(100) UNIQUE,
-    salary INT
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL
 );
