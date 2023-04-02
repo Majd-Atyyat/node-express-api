a1188a1f1612a0f09191283ae4842d48eccb04f2
majd atyyat <majd.atyyat@pwc.com>
2023-04-02 07:57:48 +0300

    updating code to fix  a bug

--- index.js
+++ index.js
@@ -42,13 +42,14 @@ app.post('/items', async (req, res) => {
 
 //  Updating an item by ID
 app.put('/items/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const item = await prisma.item.update({ where: { id } });
-  if (!item) {
-    return res.status(404).json({ error: 'Item not found' });
-  }
-  const updatedItem = await prisma.item.update({ where: { id }, data: { name } });
+  const id = parseInt(req.params.id);
+  const { name, description, price } = req.body;
+  
+  const updatedItem = await prisma.item.update({
+    where: { id },
+    data: { name, description, price },
+  });
+  
   res.json(updatedItem);
 });
 
