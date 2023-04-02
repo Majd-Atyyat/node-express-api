b893e121017f85072a235872ecc632d8c9741257
majd atyyat <majd.atyyat@pwc.com>
2023-04-02 05:48:36 +0300

    changed the table in db and the routes, deleted the env file

--- index.js
+++ index.js
@@ -1,84 +1,92 @@
-const express = require("express");
-const { Pool } = require("pg");
-require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const pg = require('pg');
 
 const app = express();
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
-
-app.use(express.json());
-
-// get all employees
-app.get("/", async (req, res) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM employee");
-    res.json(rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-//get specific employee
-app.get('/emp_id', async (req, res) => {
-  const emp_id = req.params.emp_id; // Get the ID from the URL parameter
-  try {
-    const { rows } = await pool.query('SELECT * FROM employee WHERE emp_id = $1', [emp_id]);
-    if (rows.length === 0) {
-      res.status(404).json({ message: 'Employee not found' });
-    } else {
-      res.status(200).json(rows[0]);
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-
-// add employees 
-app.post("/", async (req, res) => {
-    const { first_name , last_name,gender,birthdate,email,salary } = req.body;
-  try {
-    await pool.query("INSERT INTO employee (first_name , last_name,gender,birthdate,email,salary) VALUES ($1, $2,$3,$4,$5,$6)", [first_name , last_name,gender,birthdate,email,salary]);
-    res.json({ message: "Data created successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-// edit employee
-app.put("/:emp_id", async (req, res) => {
-    const { emp_id } = req.params;
-  const { first_name , last_name,gender,birthdate,email,salary } = req.body;
-    try {
-      await pool.query("UPDATE employee SET first_name = $1, last_name = $2, gender = $1, birthdate = $2, email = $1, salary = $2 WHERE emp_id = $3", [first_name, last_name,gender,birthdate,email,salary,emp_id]);
-      res.json({ message: "Data updated successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  });
-  
-//   delete employee
-  app.delete("/:emp_id", async (req, res) => {
-    const emp_id = req.params.emp_id;
-    try {
-      await pool.query("DELETE FROM users WHERE emp_id = $1", [emp_id]);
-      res.json({ message: "Data deleted successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  });
-  
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+
+const config = {
+   user: "postgres",
+    password: "zatar123!",
+    host: "interns.postgres.database.azure.com",
+    port: 5432,
+    database: "majd",
+};
+
+const pool = new pg.Pool(config);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// GET all items
+app.get('/items', (req, res) => {
+  pool.query('SELECT * FROM items', (err, result) => {
+    if (err) {
+      throw err;
+    }
+
+    res.send(result.rows);
+  });
 });
+
+// GET one item by id
+app.get('/items/:id', (req, res) => {
+  const id = req.params.id;
+
+  pool.query(`SELECT * FROM items WHERE id = ${id}`, (err, result) => {
+    if (err) {
+      throw err;
+    }
+
+    res.send(result.rows);
+  });
+});
+
+// POST a new item
+app.post('/items', (req, res) => {
+  const { name, description, price } = req.body;
+
+  pool.query(
+    `INSERT INTO items (name, description, price) VALUES ('${name}', '${description}', ${price})`,
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      res.send(`Item added with ID: ${result.insertId}`);
+    }
+  );
+});
+
+// PUT (update) an existing item
+app.put('/items/:id', (req, res) => {
+  const id = req.params.id;
+  const { name, description, price } = req.body;
+
+  pool.query(
+    `UPDATE items SET name = '${name}', description = '${description}', price = ${price} WHERE id = ${id}`,
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      res.send(`Item with ID: ${id} updated`);
+    }
+  );
+});
+
+// DELETE an existing item
+app.delete('/items/:id', (req, res) => {
+  const id = req.params.id;
+
+  pool.query(`DELETE FROM items WHERE id = ${id}`, (err, result) => {
+    if (err) {
+      throw err;
+    }
+
+    res.send(`Item with ID: ${id} deleted`);
+  });
+});
+
+// Start the server
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
